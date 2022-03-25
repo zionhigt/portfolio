a00fd2e372380e0bc9542aeaefd65b9a2be074a7
zionhigt <zion@SHADOW-BO4F2QG2.localdomain>
2022-03-25 17:47:36 +0100

    CORS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Messages = require('./models/messages/messages');
 const TMDB = require('./models/TMDB/model');
 
 app.use((req, res, next)=>{
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'https://zionhigt.github.io');
 	res.setHeader('Access-Control-Allow-Headers', 'x-www-urlencode, x-Content-Type,  Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
