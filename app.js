0057066bb712e2fde311a786126a13cd4fe37951
zionhigt <zion@SHADOW-BO4F2QG2.localdomain>
2022-03-27 10:23:56 +0200

    move cors to env

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const fetch = require('fetch');
-const fs = require("fs");
 
 app.use((req, res, next)=>{
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000'); //https://zionhigt.github.io
+	res.setHeader('Access-Control-Allow-Origin', process.env.AccessControlAllowOrigin); //https://zionhigt.github.io
 	res.setHeader('Access-Control-Allow-Headers', 'x-www-urlencode, x-Content-Type,  Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
